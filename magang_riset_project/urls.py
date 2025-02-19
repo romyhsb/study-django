3eeccc9055f4e5657f6f94b65a650498ce642ab1
Romi <romyhsb16072004@gmail.com>
2025-02-19 15:30:58 +0700

    menambahkan dashboard apps dengan flexmonster

--- magang_riset_project/urls.py
+++ magang_riset_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.data_dosen.urls')),
     path('', include('apps.mata_kuliah.urls')),
+    path("", include('apps.dashboard.urls'))
 ]
